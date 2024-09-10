fd2519b7872ea44a885cbc41d79675f48ff984f1
Naveen Kumar <naveensmart680@gmail.com>
2024-09-10 20:54:21 +0000

    push code

--- Basics/AssessmentOne.js
+++ Basics/AssessmentOne.js
@@ -1,6 +1,6 @@
-// Assign value a and b and check the assigment a is equals to b print boolean value  as result
+/* Assign value a and b and check the assigment a is equals to b print boolean value  as result */
 
-/*let a = 89;
+let a = 89;
 let b = 11;
 
 if (a === b){
@@ -10,8 +10,8 @@ if (a === b){
 else {
     console.log(false);
              
-    }   */
+    }   
     
    // what is the output of ?
 
-   console.log(5>10 || console.log("Hello"));
+  // console.log(5>10 || console.log("Hello"));
